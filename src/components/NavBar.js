78810b69eed19ae7219a50047fb0236e83f49e15
Noah Eakin <noaheakin@gmail.com>
2021-03-18 15:01:15 -0700

    updated package depencies to include typescript, gatsby-plugins-typescript

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import logo from '../images/walkie-logo.png';
-const activeStyles = {
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+var jsx_runtime_1 = require("react/jsx-runtime");
+var gatsby_1 = require("gatsby");
+var walkie_logo_png_1 = require("../images/walkie-logo.png");
+var activeStyles = {
     background: 'red'
 };
-const NavBar = () => {
-    return (React.createElement("div", { className: "navbar" },
-        React.createElement("div", { className: "container h-8 flex flex-row mx-auto my-5" },
-            React.createElement(Link, { to: "/" },
-                React.createElement("img", { className: "h-8", src: logo })),
-            React.createElement("div", { className: "flex-grow" }),
-            React.createElement("div", { className: "justify-between" },
-                React.createElement(Link, { id: "navLink", to: "/features", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, "Features"),
-                React.createElement(Link, { id: "navLink", to: "/roadmap", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, "Roadmap"),
-                React.createElement(Link, { id: "navLink", to: "/contact", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, "Contact")))));
+var NavBar = function () {
+    return (jsx_runtime_1.jsx("div", __assign({ className: "navbar" }, { children: jsx_runtime_1.jsxs("div", __assign({ className: "container h-8 flex flex-row mx-auto my-5" }, { children: [jsx_runtime_1.jsx(gatsby_1.Link, __assign({ to: "/" }, { children: jsx_runtime_1.jsx("img", { className: "h-8", src: walkie_logo_png_1["default"] }, void 0) }), void 0),
+                jsx_runtime_1.jsx("div", { className: "flex-grow" }, void 0),
+                jsx_runtime_1.jsxs("div", __assign({ className: "justify-between" }, { children: [jsx_runtime_1.jsx(gatsby_1.Link, __assign({ id: "navLink", to: "/features", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, { children: "Features" }), void 0),
+                        jsx_runtime_1.jsx(gatsby_1.Link, __assign({ id: "navLink", to: "/roadmap", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, { children: "Roadmap" }), void 0),
+                        jsx_runtime_1.jsx(gatsby_1.Link, __assign({ id: "navLink", to: "/contact", className: "text-gray-500 hover:text-gray-900 mx-1 px-5" }, { children: "Contact" }), void 0)] }), void 0)] }), void 0) }), void 0));
 };
-export default NavBar;
+exports["default"] = NavBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
-import * as screenImg from '../images/walkie-screen.jpg';
-import closerImg from '../images/closer-together.png';
-import distractionImg from '../images/distraction-free.png';
-import voiceImg from '../images/voice-only.png';
-const App = () => {
-    return (React.createElement("div", { className: "App" },
-        React.createElement(NavBar, null),
-        React.createElement("div", { className: "flex pt-8 bg-white mt-28 mx-auto space-y-2 xs:py-0 xs:flex-col xs:items-center xs:space-y-0" },
-            React.createElement("div", { className: "sm:order-10 w-1/2 text-left px-20" },
-                React.createElement("h1", { className: "text-4xl py-5 font-extrabold tracking-wide leading-snug" }, "Simple, real time voice chat for your remote team."),
-                React.createElement("h3", { className: "text-xl" }, "Walkie is a better way to communicate for distributed teams that don't want to spend all day in dry text channels."),
-                React.createElement("form", { className: "my-10" },
-                    React.createElement("input", { className: "border border-r-0 rounded-l-lg py-3 border-gray-300", type: "text", placeholder: "   > work email", required: true }),
-                    React.createElement("button", { className: "border border-black bg-black text-white px-3 py-3 font-bold rounded-r-lg" }, "Become Founding User"))),
-            React.createElement("img", { className: "md:w-1/2", src: screenImg })),
-        React.createElement("div", { className: "bg-black flex py-20 justify-items-center px-14 text-white" },
-            React.createElement("div", { className: "flex flex-col space-y-3 pr-12" },
-                React.createElement("img", { className: "h-15 w-20", src: voiceImg }),
-                React.createElement("h2", { className: "text-2xl" }, "Voice Only"),
-                React.createElement("p", null, "A voice only communication tool that enables teams to communicate with emotion and purpose.")),
-            React.createElement("div", { className: "flex flex-col space-y-3 pr-12" },
-                React.createElement("img", { className: "h-14 w-14", src: distractionImg }),
-                React.createElement("h2", { className: "text-2xl" }, "Distraction Free"),
-                React.createElement("p", null, "No more \u2318-tabbing. Spend less time in slack channels and more time on the work that actually matters.")),
-            React.createElement("div", { className: "flex flex-col space-y-3" },
-                React.createElement("img", { className: "h-15 w-20", src: closerImg }),
-                React.createElement("h2", { className: "text-2xl" }, "Closer Together"),
-                React.createElement("p", null, "Subtle hints of presence so that even the most distributed teams feel connected across continents."))),
-        React.createElement("div", { className: "flex flex-column" },
-            React.createElement("div", { className: "flex" },
-                React.createElement("div", { className: "flex flex-column" },
-                    React.createElement("h2", null, "Have fewer meetings and make faster decisions"),
-                    React.createElement("p", null, "Walkie keeps people out of unnecessary meetings by providing the value of real time communication with out the need for everyone to drop everything.")),
-                React.createElement("img", { src: process.env.PUBLIC_URL + '/images/request-access1.png' })),
-            React.createElement("div", { className: "flex" },
-                React.createElement("img", { src: process.env.PUBLIC_URL + '/images/send-us-feedback.jpg' })),
-            React.createElement("div", null,
-                React.createElement("img", { src: process.env.PUBLIC_URL + '/images/request-access2.jpg' })))));
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-export default App;
+exports.__esModule = true;
+var jsx_runtime_1 = require("react/jsx-runtime");
+var NavBar_1 = require("../components/NavBar");
+var screenImg = require("../images/walkie-screen.jpg");
+var closer_together_png_1 = require("../images/closer-together.png");
+var distraction_free_png_1 = require("../images/distraction-free.png");
+var voice_only_png_1 = require("../images/voice-only.png");
+var App = function () {
+    return (jsx_runtime_1.jsxs("div", __assign({ className: "App" }, { children: [jsx_runtime_1.jsx(NavBar_1["default"], {}, void 0),
+            jsx_runtime_1.jsxs("div", __assign({ className: "flex pt-8 bg-white mt-28 mx-auto space-y-2 xs:py-0 xs:flex-col xs:items-center xs:space-y-0" }, { children: [jsx_runtime_1.jsxs("div", __assign({ className: "sm:order-10 w-1/2 text-left px-20" }, { children: [jsx_runtime_1.jsx("h1", __assign({ className: "text-4xl py-5 font-extrabold tracking-wide leading-snug" }, { children: "Simple, real time voice chat for your remote team." }), void 0),
+                            jsx_runtime_1.jsx("h3", __assign({ className: "text-xl" }, { children: "Walkie is a better way to communicate for distributed teams that don't want to spend all day in dry text channels." }), void 0),
+                            jsx_runtime_1.jsxs("form", __assign({ className: "my-10" }, { children: [jsx_runtime_1.jsx("input", { className: "border border-r-0 rounded-l-lg py-3 border-gray-300", type: "text", placeholder: "   > work email", required: true }, void 0),
+                                    jsx_runtime_1.jsx("button", __assign({ className: "border border-black bg-black text-white px-3 py-3 font-bold rounded-r-lg" }, { children: "Become Founding User" }), void 0)] }), void 0)] }), void 0),
+                    jsx_runtime_1.jsx("img", { className: "md:w-1/2", src: screenImg }, void 0)] }), void 0),
+            jsx_runtime_1.jsxs("div", __assign({ className: "bg-black flex py-20 justify-items-center px-14 text-white" }, { children: [jsx_runtime_1.jsxs("div", __assign({ className: "flex flex-col space-y-3 pr-12" }, { children: [jsx_runtime_1.jsx("img", { className: "h-15 w-20", src: voice_only_png_1["default"] }, void 0),
+                            jsx_runtime_1.jsx("h2", __assign({ className: "text-2xl" }, { children: "Voice Only" }), void 0),
+                            jsx_runtime_1.jsx("p", { children: "A voice only communication tool that enables teams to communicate with emotion and purpose." }, void 0)] }), void 0),
+                    jsx_runtime_1.jsxs("div", __assign({ className: "flex flex-col space-y-3 pr-12" }, { children: [jsx_runtime_1.jsx("img", { className: "h-14 w-14", src: distraction_free_png_1["default"] }, void 0),
+                            jsx_runtime_1.jsx("h2", __assign({ className: "text-2xl" }, { children: "Distraction Free" }), void 0),
+                            jsx_runtime_1.jsx("p", { children: "No more \u2318-tabbing. Spend less time in slack channels and more time on the work that actually matters." }, void 0)] }), void 0),
+                    jsx_runtime_1.jsxs("div", __assign({ className: "flex flex-col space-y-3" }, { children: [jsx_runtime_1.jsx("img", { className: "h-15 w-20", src: closer_together_png_1["default"] }, void 0),
+                            jsx_runtime_1.jsx("h2", __assign({ className: "text-2xl" }, { children: "Closer Together" }), void 0),
+                            jsx_runtime_1.jsx("p", { children: "Subtle hints of presence so that even the most distributed teams feel connected across continents." }, void 0)] }), void 0)] }), void 0),
+            jsx_runtime_1.jsxs("div", __assign({ className: "flex flex-column" }, { children: [jsx_runtime_1.jsxs("div", __assign({ className: "flex" }, { children: [jsx_runtime_1.jsxs("div", __assign({ className: "flex flex-column" }, { children: [jsx_runtime_1.jsx("h2", { children: "Have fewer meetings and make faster decisions" }, void 0),
+                                    jsx_runtime_1.jsx("p", { children: "Walkie keeps people out of unnecessary meetings by providing the value of real time communication with out the need for everyone to drop everything." }, void 0)] }), void 0),
+                            jsx_runtime_1.jsx("img", { src: process.env.PUBLIC_URL + '/images/request-access1.png' }, void 0)] }), void 0),
+                    jsx_runtime_1.jsx("div", __assign({ className: "flex" }, { children: jsx_runtime_1.jsx("img", { src: process.env.PUBLIC_URL + '/images/send-us-feedback.jpg' }, void 0) }), void 0),
+                    jsx_runtime_1.jsx("div", { children: jsx_runtime_1.jsx("img", { src: process.env.PUBLIC_URL + '/images/request-access2.jpg' }, void 0) }, void 0)] }), void 0)] }), void 0));
+};
+exports["default"] = App;
