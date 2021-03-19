5ce1cdce9ed90277c7ef981fa125c6ba954269e3
Noah Eakin <noaheakin@gmail.com>
2021-03-18 17:25:29 -0700

    cleaned up package dependencies to remove ts redundencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 }
